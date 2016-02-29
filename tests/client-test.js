c73b7f3d69bdd5e629457ea6acc79dbfac82cb23
Jonathan Torres <jonathan@nobox.com>
2016-02-29 12:40:31 -0500

    Update test.

--- tests/client-test.js
+++ tests/client-test.js
@@ -22,32 +22,14 @@ describe('Finalizer client', function() {
     });
 
     it('should create a new project on the build server', function(done) {
+        var jsonResponse = { msg: 'Project "my-project" created' };
         sinon.stub(request, 'post')
-            .yields(null, { statusCode: 200 }, 'Project "my-project" created');
+            .yields(null, { statusCode: 200 }, JSON.stringify(jsonResponse));
 
         client.create('my-project', function(err, body) {
             expect(request.post.called).to.be.ok();
             expect(request.post.callCount).to.be(1);
             expect(body).to.be('Project "my-project" created');
-            done();
-        });
-    });
-
-    it('should download the latest build from the build server', function(done) {
-        sinon.stub(request, 'post')
-            .yields(null, { statusCode: 200 }, 'File with contents')
-            .returns({ on: function() {}, pipe: function() {}});
-
-        sinon.stub(fs, 'createWriteStream');
-        sinon.stub(childProcess, 'spawn')
-            .returns({ on: function(s) {}});
-        sinon.stub(tar, 'Extract')
-            .returns({ on: function(s) {}});
-
-        client.download('my-project', function(body) {
-            expect(request.post.called).to.be.ok();
-            expect(request.post.callCount).to.be(1);
-            expect(body).to.be('Downloaded and rebuilt dependencies for "my-project"');
             done();
         });
     });
